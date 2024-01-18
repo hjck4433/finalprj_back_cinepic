f53f28b7fdbf284ab71a9989c7f47d26af44f031
pshyun123 <pshyun7204@gmail.com>
2024-01-18 11:26:12 +0900

    소현 faq,bookmark repository 작성

--- src/main/java/com/kh/cinepic/repository/AdminRepository.java
+++ src/main/java/com/kh/cinepic/repository/AdminRepository.java
@@ -4,4 +4,5 @@ import com.kh.cinepic.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin, String> {
+
 }
---
+++ src/main/java/com/kh/cinepic/repository/BookmarkRepository.java
@@ -0,0 +1,18 @@
+package com.kh.cinepic.repository;
+
+import com.kh.cinepic.entity.Bookmark;
+import com.kh.cinepic.entity.Member;
+import com.kh.cinepic.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BookmarkRepository extends JpaRepository <Bookmark, Long> {
+    boolean existByMemberAndMovie(Member member, Movie movie);
+    Optional<Bookmark> findByMemberAndMovie(Member member, Movie movie);
+    Page<Bookmark> findByMember(Member member, Pageable pageable);
+}
