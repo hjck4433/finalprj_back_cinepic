5dfa715a0cc192792eb0f2a5cb611a8beded997a
hjck4433 <hjck4433@gmail.com>
2024-01-19 08:25:02 +0900

    현지 MemberService 일부 추가

---
+++ src/main/java/com/kh/cinepic/service/MemberService.java
@@ -0,0 +1,88 @@
+package com.kh.cinepic.service;
+
+import com.kh.cinepic.dto.MemberReqDto;
+import com.kh.cinepic.entity.Kakao;
+import com.kh.cinepic.entity.Member;
+import com.kh.cinepic.entity.RefreshToken;
+import com.kh.cinepic.repository.KakaoRepository;
+import com.kh.cinepic.repository.MemberRepository;
+import com.kh.cinepic.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final KakaoRepository kakaoRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    // 비밀번호 일치 체크
+    public boolean isPassword(String password, Long id) {
+        log.info("password : {}", password);
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("해당 회원이 존재하지 않습니다."));
+        boolean isPw = passwordEncoder.matches(password, member.getPassword());
+        log.info("isPw : {}", isPw);
+        return isPw;
+    }
+
+    // 회원 정보 수정
+    public boolean modifyMember(MemberReqDto memberReqDto) {
+        log.info("password : {}", memberReqDto.getPassword());
+        try {
+            Member member = memberRepository.findByEmail(memberReqDto.getEmail())
+                    .orElseThrow(() -> new RuntimeException("해당 회원이 존재하지 않습니다."));
+            // 카카오 회원인 경우 비밀번호 수정 X
+            log.info("카카오 회원? : {}", member.isKakao());
+            if(!member.isKakao() && !memberReqDto.getPassword().isEmpty()){
+                member.setPassword(passwordEncoder.encode(memberReqDto.getPassword()));
+            }
+            member.setAlias(memberReqDto.getAlias());
+            member.setPhone(memberReqDto.getPhone());
+            member.setAddr(memberReqDto.getAddr());
+            member.setImage(memberReqDto.getImage());
+            memberRepository.save(member);
+            return true;
+        }catch(Exception e) {
+            log.error("회원 정보 수정 중 오류 발생");
+            return false;
+        }
+    }
+
+    // 회원 탈퇴
+    public boolean withdrawMember(Long id){
+        try {
+            Member member = memberRepository.findById(id)
+                    .orElseThrow(() -> new RuntimeException("해당 회원이 존재하지 않습니다."));
+            member.setWithdraw(true);
+
+            if(kakaoRepository.existsByEmail(member.getEmail())){
+                Kakao kakao = kakaoRepository.findByEmail(member.getEmail())
+                        .orElseThrow(() -> new RuntimeException("카카오 회원 정보가 없습니다."));
+                kakaoRepository.delete(kakao);
+            }
+
+            member.setEmail(member.getEmail().concat("-"));
+            member.setPhone("");
+            member.setAddr("");
+            member.setName("");
+            memberRepository.save(member);
+
+            RefreshToken refreshToken = refreshTokenRepository.findByMember(member)
+                    .orElseThrow(() -> new RuntimeException("Token정보가 업습니다."));
+            refreshTokenRepository.delete(refreshToken);
+
+            return true;
+        }catch (Exception e){
+            log.error("회원 탈퇴 처리 중 오류 발생");
+            return false;
+        }
+    }
+}
