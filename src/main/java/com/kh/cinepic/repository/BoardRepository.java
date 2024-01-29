d0ac019064a2fe887f27a45243879240096b5a18
pshyun123 <pshyun7204@gmail.com>
2024-01-29 10:43:45 +0900

    소현 admin board 관련 repository 수정

--- src/main/java/com/kh/cinepic/repository/BoardRepository.java
+++ src/main/java/com/kh/cinepic/repository/BoardRepository.java
@@ -1,7 +1,14 @@
 package com.kh.cinepic.repository;
 
 import com.kh.cinepic.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    // 관리자 - board 모든 정보 가져오기
+    Page<Board> findAll(Pageable pageable);
 }
