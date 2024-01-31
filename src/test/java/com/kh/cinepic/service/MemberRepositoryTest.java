557699136581f5cd2051d6165fa041e00f4b1630
11yhj <uouo_@naver.com>
2024-01-31 11:52:21 +0900

    현주 MemberRepositoryTest 추가

---
+++ src/test/java/com/kh/cinepic/service/MemberRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.kh.cinepic.service;
+
+
+import com.kh.cinepic.entity.Member;
+import com.kh.cinepic.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@Transactional
+@Slf4j
+@RequiredArgsConstructor
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+    @PersistenceContext
+    EntityManager em;
+
+    public Member createMemberInfo() {
+        Member member = new Member();
+        member.setEmail("test@gmail.com");
+        member.setPassword("test1234!");
+        member.setName("테스트");
+        member.setAlias("다람쥐");
+        member.setPhone("010-1111-2222");
+        member.setAddr("서울시 강남구 역삼동");
+        member.setRegDate(LocalDateTime.now());
+        return member;
+    }
+
+
+    @Test
+    @DisplayName("회원 정보 저장 테스트")
+    public void saveMemberTest(){
+        Member member = createMemberInfo();
+        Member testMember = memberRepository.save(member);
+        em.flush();
+        em.clear();
+        System.out.println("testMember 결과 : " + testMember);
+    }
+}
